143ce0454059dd8502f2c43cf6a491facec36748
mikub97 <mikub97@gmail.com>
2023-06-08 20:55:06 +0200

    agent data analysis notebook added

--- src/traffic/main.py
+++ src/traffic/main.py
@@ -4,7 +4,7 @@ from src.traffic.gui import GUI
 from src.traffic.model import TrafficModel
 
 
-experiment = "3_lanes_no_lights_5_cars"
+experiment = "2_lane_no_lights_20_cars"
 
 gui = GUI(TrafficModel(
          experiment=experiment
