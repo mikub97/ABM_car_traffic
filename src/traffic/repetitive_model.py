18c862b08cf214ab72d3c991565c94d4a2e6c0ab
mikub97 <mikub97@gmail.com>
2023-06-25 14:44:52 +0200

    clean up

--- src/traffic/repetitive_model.py
+++ src/traffic/repetitive_model.py
@@ -5,19 +5,22 @@ import pandas as pd
 from src.traffic.analysis_utils import collect_data
 from src.traffic.driver import Driver
 from src.traffic.model import TrafficModel
-x_sessions =  [{
-                "n_agents": 25,
-                "max_speed_avg": v,
-                "max_speed_dev": 0.4,
 
-                "desired_distance_avg": 30,
-                "desired_distance_dev": 0,
+x_sessions = [
+    {
+        "n_agents": 25,
+        "max_speed_avg": v,
+        "max_speed_dev": 0.4,
 
-                "acceleration_avg": 1.5,
-                "acceleration_dev": 0.3,
-                "n_lanes":3
+        "desired_distance_avg": 30,
+        "desired_distance_dev": 0,
 
-            } for v in [0.75,1,1.25,1.5,1.75,2]]
+        "acceleration_avg": 1.5,
+        "acceleration_dev": 0.3,
+        "n_lanes":3
+
+    }
+    for v in [0.75,1,1.25,1.5,1.75,2]]
 class RepetitiveTrafficModel(TrafficModel):
 
     def __init__(self, experiment):
@@ -25,20 +28,6 @@ class RepetitiveTrafficModel(TrafficModel):
         self.measures_files = "output_files/" + experiment + "/measures_files.csv"
         self.finished = False
         self.sessions = x_sessions
-        # [
-        #     {
-        #         "n_agents": 20,
-        #         "max_speed_avg": 1.5,
-        #         "max_speed_dev": 0.3,
-        #
-        #         "desired_distance_avg": 30,
-        #         "desired_distance_dev": 3,
-        #
-        #         "acceleration_avg": 1.5,
-        #         "acceleration_dev": 0,
-        #         "n_lanes":3
-        #     }
-        # ]
         self.session_times = []
         self.agentID_counter = 0
         self.session_counter = 0
